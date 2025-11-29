f4bb2e0ab9a9fc42d693dd331c26f7ad5de1a36f
Kevin Sirait <siraitkevin25@gmail.com>
2025-11-30 04:43:37 +0700

    bug fixing id

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -32,7 +32,7 @@ return new class extends Migration
         });
 
         Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
+            $table->string('uid')->primary();
             $table->string('user_id')->nullable()->index(); // Ubah di sini
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
