bd08435f15e7624e7f4df4892d806b07a1359f2b
kevinnsrt <siraitkevin25@gmail.com>
2025-12-02 04:30:50 +0700

    cart and product relation

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Models\User;
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Cart extends Model
 {
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Models\Cart;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
