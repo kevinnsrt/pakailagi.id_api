ed3a3c7f2b5af86a8ca5ae04b4fcc62db5ebcb99
kevinnsrt <siraitkevin25@gmail.com>
2025-12-02 04:27:41 +0700

    cart and product relation

--- routes/api.php
+++ routes/api.php
@@ -22,9 +22,11 @@ Route::post('/login', [FirebaseAuthController::class, 'getUserdata']);
 
 // carts
 Route::post('/carts', [CartController::class, 'store']);
-Route::post('/carts/user', [CartController::class, 'show']);
 
 });
+
+// testing tanpa token
+Route::post('/carts/user', [CartController::class, 'show']);
 
 
 
