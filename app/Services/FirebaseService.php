ef86824b587ea7a46e180cef841f46dd655dada8
kevinnsrt <siraitkevin25@gmail.com>
2025-12-16 17:28:55 +0700

    subscribe notification

--- app/Services/FirebaseService.php
+++ app/Services/FirebaseService.php
@@ -18,7 +18,9 @@ class FirebaseService
     {
         $credentials = new ServiceAccountCredentials(
             'https://www.googleapis.com/auth/firebase.messaging',
-            json_decode(file_get_contents(storage_path('app/cred.json')), true)
+            json_decode(file_get_contents(storage_path('app/cred.json')), true),
+            dd(json_decode(file_get_contents(storage_path('app/firebase.json')), true))
+
         );
 
         $token = $credentials->fetchAuthToken();
