b9d7ccea6728a8637a8ddfadc925017fed1afe31
samuelgautama <maniksamuel117107091@gmail.com>
2025-12-17 16:48:38 +0700

    update ui

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,31 +1,38 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
 <body class="font-sans text-gray-900 antialiased bg-gray-50">
-        <div class="min-h-screen flex flex-col justify-center items-center pt-0 bg-gray-50 px-4">
-            
-            <div class="mb-6">
-                <a href="/">
-                    <img src="{{ asset('images/logo.png') }}" alt="Logo" class="h-20 w-auto drop-shadow-sm" />
-                </a>
-            </div>
+    <div class="min-h-screen flex flex-col justify-center items-center pt-0 bg-gray-50 px-4">
 
-            <div class="w-full sm:max-w-md px-6 py-8 bg-white shadow-xl overflow-hidden sm:rounded-2xl border border-gray-100">
-                {{ $slot }}
-            </div>
+        <div class="mb-6">
+            <a href="/">
+                <img src="{{ asset('images/logo.png') }}" alt="Logo" class="h-20 w-auto drop-shadow-sm" />
+            </a>
         </div>
-    </body>
+
+        <div class="w-full sm:max-w-md px-6 py-8 bg-white shadow-xl overflow-hidden sm:rounded-2xl border border-gray-100">
+            {{ $slot }}
+        </div>
+    </div>
+</body>
+
 </html>
