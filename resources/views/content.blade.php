e9e443931361318829687d49c895156f4ca6eb48
samuelgautama <maniksamuel117107091@gmail.com>
2025-12-17 19:38:16 +0700

    delete ui

--- resources/views/content.blade.php
+++ resources/views/content.blade.php
@@ -189,7 +189,7 @@
                         Ya, Hapus
                     </button>
                     <button type="button" onclick="closeDeleteModal()" 
-                        class="mt-3 sm:mt-0 inline-flex w-full justify-center items-center rounded-lg bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-300 sm:w-auto transition-all">
+                        class="inline-flex w-full justify-center items-center rounded-lg bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-300 sm:w-auto transition-all">
                         Batal
                     </button>
                 </div>
